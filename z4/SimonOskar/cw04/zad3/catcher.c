b3d8e83481a7a87c2a43781837f73e45c8303080
Oskar <oskarsimon3@gmail.com>
2022-04-09 12:23:34 +0200

    z4z3 zrobione, ale nie dziala

--- z4/SimonOskar/cw04/zad3/catcher.c
+++ z4/SimonOskar/cw04/zad3/catcher.c
@@ -4,16 +4,54 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+int sigcount = 0;
+int end_of_receiving = 0;
+pid_t sender_pid;
+
+void handler(int signum, siginfo_t *info, void *ucontext_t)
+{
+    switch (signum)
+    {
+    case SIGUSR1:
+        sigcount++;
+        break;
+    case SIGUSR2:
+        end_of_receiving = 1;
+        sender_pid = info->si_pid;
+        break;
+    default:
+        break;
+    }
+}
+
 int main(int argc, char **argv)
 {
     printf("Catcher's PID: %d\n", getpid());
+
+    struct sigaction act;
+    act.sa_flags = SA_SIGINFO;
+    act.sa_sigaction = handler;
+    sigemptyset(&act.sa_mask);
+    sigaction(SIGUSR1, &act, NULL);
+    sigaction(SIGUSR2, &act, NULL);
+
     sigset_t mask;
     sigfillset(&mask);
     sigdelset(&mask, SIGUSR1);
     sigdelset(&mask, SIGUSR2);
-    if (sigprocmask(SIG_BLOCK, &mask, NULL) < 0)
-            perror("Nie udalo sie zablokowac sygnalow");
+
+    printf("Initializing sender...\n");
     execl("./sender", "./sender", getpid(), argv[1], NULL);
+
+    while (!end_of_receiving)
+        sigsuspend(&mask);
+
+    printf("Received SIGUSR2\nSending SIGUSR 1 back...");
+
+    for (int i = 0; i < sigcount; i++)
+        kill(sender_pid, SIGUSR1);
+
+    kill(sender_pid, SIGUSR2);
 
     return 0;
 }
--- z4/SimonOskar/cw04/zad3/sender.c
+++ z4/SimonOskar/cw04/zad3/sender.c
@@ -4,14 +4,52 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+int sigcount = 0;
+int end_of_receiving = 0;
+
+void handler(int signum)
+{
+    switch (signum)
+    {
+    case SIGUSR1:
+        sigcount++;
+        break;
+    case SIGUSR2:
+        end_of_receiving = 1;
+        break;
+    default:
+        break;
+    }
+}
+
 int main(int argc, char **argv)
 {
+    printf("Sender is alive\nSender's PID: %d\n", getpid());
     int num_of_signals = atoi(argv[2]);
     pid_t catcher_pid = atoi(argv[1]);
+
+    struct sigaction act;
+    act.sa_handler = handler;
+    sigemptyset(&act.sa_mask);
+    sigaction(SIGUSR1, &act, NULL);
+    sigaction(SIGUSR2, &act, NULL);
+
+    sigset_t mask;
+    sigfillset(&mask);
+    sigdelset(&mask, SIGUSR1);
+    sigdelset(&mask, SIGUSR2);
+
+    printf("Sending SIGUSR1...\n");
     for (int i = 0; i < num_of_signals; i++)
-    {
         kill(catcher_pid, SIGUSR1);
-    }
+
+    printf("Sending SIGUSR2\n");
     kill(catcher_pid, SIGUSR2);
+
+    while (!end_of_receiving)
+        sigsuspend(&mask);
+
+    printf("Sender sent %d signals\nReceived %d signals\n",
+           num_of_signals, sigcount);
     return 0;
 }
