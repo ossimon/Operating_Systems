8814a57bb058c19396b2139cde9159c2cc4aec39
Oskar <oskarsimon3@gmail.com>
2022-04-09 13:28:47 +0200

    z4z3a modes done, sig1 isn't registered

--- z4/SimonOskar/cw04/zad3/catcher.c
+++ z4/SimonOskar/cw04/zad3/catcher.c
@@ -3,18 +3,20 @@
 #include <signal.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <string.h>
 
 int sigcount = 0;
 int end_of_receiving = 0;
 pid_t sender_pid;
 
+
 int translate_mode(char *mode_name)
 {
-    if (mode_name == "kill")
+    if (strcmp(mode_name, "kill") == 0)
         return 0;
-    else if (mode_name == "sigqueue")
+    else if (strcmp(mode_name, "sigqueue") == 0)
         return 1;
-    else if (mode_name == "sigrt")
+    else if (strcmp(mode_name, "sigrt") == 0)
         return 2;
     else
     {
@@ -36,26 +38,28 @@ void handler(int signum, siginfo_t *info, void *ucontext_t)
 int main(int argc, char **argv)
 {
     printf("Catcher's PID: %d\n", getpid());
-
     int mode = translate_mode(argv[1]);
+
     struct sigaction act;
     act.sa_flags = SA_SIGINFO;
     act.sa_sigaction = handler;
     sigemptyset(&act.sa_mask);
-    sigaction(SIGUSR1, &act, NULL);
-    sigaction(SIGUSR2, &act, NULL);
-
     sigset_t mask;
     sigfillset(&mask);
+
     if (mode == 2)
     {
         sigdelset(&mask, SIGRTMIN);
         sigdelset(&mask, SIGRTMAX);
+        sigaction(SIGRTMIN, &act, NULL);
+        sigaction(SIGRTMAX, &act, NULL);
     }
     else
     {
         sigdelset(&mask, SIGUSR1);
         sigdelset(&mask, SIGUSR2);
+        sigaction(SIGUSR1, &act, NULL);
+        sigaction(SIGUSR2, &act, NULL);
     }
 
     printf("Waiting for sender\n");
@@ -63,9 +67,14 @@ int main(int argc, char **argv)
     while (!end_of_receiving)
         sigsuspend(&mask);
 
-    printf("Received SIGUSR1 %d times\nReceived SIGUSR2\nSending SIGUSR 1 back...\n",
+    if (mode == 2)
+        printf("Received SIGRTMIN %d times\nReceived SIGRTMAX\nSending SIGRTMAX back...\n",
            sigcount);
-
+    else
+        printf("Received SIGUSR1 %d times\nReceived SIGUSR2\nSending SIGUSR1 back...\n",
+           sigcount);
+    
+    union sigval value;
     switch (mode)
     {
     case 0:
@@ -75,7 +84,6 @@ int main(int argc, char **argv)
         kill(sender_pid, SIGUSR2);
         break;
     case 1:
-        union sigval value;
         for (int i = 0; i < sigcount; i++)
             sigqueue(sender_pid, SIGUSR1, value);
 
--- z4/SimonOskar/cw04/zad3/sender.c
+++ z4/SimonOskar/cw04/zad3/sender.c
@@ -3,17 +3,18 @@
 #include <signal.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <string.h>
 
 int sigcount = 0;
 int end_of_receiving = 0;
 
 int translate_mode(char *mode_name)
 {
-    if (mode_name == "kill")
+    if (strcmp(mode_name, "kill") == 0)
         return 0;
-    else if (mode_name == "sigqueue")
+    else if (strcmp(mode_name, "sigqueue") == 0)
         return 1;
-    else if (mode_name == "sigrt")
+    else if (strcmp(mode_name, "sigrt") == 0)
         return 2;
     else
     {
@@ -40,22 +41,25 @@ int main(int argc, char **argv)
     struct sigaction act;
     act.sa_handler = handler;
     sigemptyset(&act.sa_mask);
-    sigaction(SIGUSR1, &act, NULL);
-    sigaction(SIGUSR2, &act, NULL);
-
     sigset_t mask;
     sigfillset(&mask);
+
     if (mode == 2)
     {
         sigdelset(&mask, SIGRTMIN);
         sigdelset(&mask, SIGRTMAX);
+        sigaction(SIGRTMIN, &act, NULL);
+        sigaction(SIGRTMAX, &act, NULL);
     }
     else
     {
         sigdelset(&mask, SIGUSR1);
         sigdelset(&mask, SIGUSR2);
+        sigaction(SIGUSR1, &act, NULL);
+        sigaction(SIGUSR2, &act, NULL);
     }
 
+    union sigval value;
     switch (mode)
     {
     case 0:
@@ -67,7 +71,6 @@ int main(int argc, char **argv)
         kill(catcher_pid, SIGUSR2);
         break;
     case 1:
-        union sigval value;
         printf("Sending SIGUSR1\n");
         for (int i = 0; i < sigcount; i++)
             sigqueue(catcher_pid, SIGUSR1, value);
