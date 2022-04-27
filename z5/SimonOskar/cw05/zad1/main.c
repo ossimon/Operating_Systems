104d43af54a77fd9bf19f055d1e9fa52e6222d76
Oskar <oskarsimon3@gmail.com>
2022-04-27 20:59:36 +0200

    z5z1 done.

--- z5/SimonOskar/cw05/zad1/main.c
+++ z5/SimonOskar/cw05/zad1/main.c
@@ -20,7 +20,7 @@ const int strquerylen = 200;
 void read_file(char *filename, char *components[complim], int queries[querylim][querylen], int lengths[2]);
 int find_index(char **comp_names, int comp_num, char *name);
 void exec_query(int *query, char **components);
-void run_query(char *query, int com_num);
+void run_query(char ***args, int com_num, int numbers_of_arguments[arglim]);
 
 int main(int argc, char **argv)
 {
@@ -57,15 +57,58 @@ int main(int argc, char **argv)
     return 0;
 }
 
-void run_query(char *query, int com_num)
+void run_query(char ***args, int com_num, int numbers_of_arguments[arglim])
 {
+    // printf("Gonna run the querry %s!\nMy pid: %d\n", args[0][0], getpid());
+    // printf("Commands to run: %d\n", com_num);
     // Pipes
     int pipes[com_num - 1][2];
     for (int i = 0; i < com_num; i++)
         pipe(pipes[i]);
 
     // Execing
-    
+    for (int i = 0; i < com_num; i++)
+    {
+        // printf("Entered for with i = %d\n", i);
+        if (fork() == 0)
+        {
+            // printf("halo, i = %d\n", i);
+            // Redirecting pipes
+            if (i == 0)
+            {
+                dup2(pipes[i][1], STDOUT_FILENO);
+            }
+            else if (i == com_num - 1)
+            {
+                dup2(pipes[i - 1][0], STDIN_FILENO);
+            }
+            else
+            {
+                dup2(pipes[i - 1][0], STDIN_FILENO);
+                dup2(pipes[i][1], STDOUT_FILENO);
+            }
+            // Closing unnecessary pipes
+            for (int j = 0; j < com_num - 1; j++)
+            {
+                if (j != i - 1)
+                    close(pipes[j][0]);
+                if (j != i)
+                    close(pipes[j][1]);
+            }
+            // Executing query components
+            for (int k = numbers_of_arguments[i]; k < arglim; k++)
+                args[i][k] = NULL;
+            execvp(args[i][0], args[i]);
+            exit(EXIT_SUCCESS);
+        }
+    }
+    for (int i = 0; i < com_num; i++)
+    {
+        close(pipes[i][0]);
+        close(pipes[i][1]);
+    }
+    for (int i = 0; i < com_num; i++)
+        wait(NULL);
 }
 
 void exec_query(int *queries, char **components)
@@ -85,11 +128,22 @@ void exec_query(int *queries, char **components)
     }
     // printf("Ready query: %s\n", query + 1);
 
-    char args[comlim][arglim][namelen];
+    // char args[comlim][arglim][namelen];
+    char ***args = malloc(complim * sizeof(char **));
     for (int i = 0; i < comlim; i++)
+    {
+        args[i] = malloc(arglim * sizeof(char *));
         for (int j = 0; j < arglim; j++)
+        {
+            args[i][j] = malloc(namelen * sizeof(char));
             for (int k = 0; k < namelen; k++)
+            {
                 args[i][j][k] = '\0';
+            }
+            // printf("!%s ", args[i][j]);
+        }
+        // printf("\n");
+    }
 
     char *command;
     char *argument;
@@ -98,8 +152,10 @@ void exec_query(int *queries, char **components)
     char *line = query;
     char *saveptr1, *saveptr2;
     int number_of_commands;
+    int numbers_of_arguments[comlim];
     for (int i = 0; i < comlim; i++, line = NULL)
     {
+        numbers_of_arguments[i] = 0;
         command = strtok_r(line, delim1, &saveptr1);
         if (command == NULL)
         {
@@ -111,22 +167,40 @@ void exec_query(int *queries, char **components)
         // printf("command: %s\n", command);
         for (int j = 0; j < arglim; j++, command = NULL)
         {
-            argument = strtok_r(command, delim2, saveptr2);
+            argument = strtok_r(command, delim2, &saveptr2);
             if (argument == NULL)
+            {
+                numbers_of_arguments[i] = j;
+                // printf("%d\n", numbers_of_arguments[i]);
                 break;
+            }
             // printf("argument: %s\n", argument);
             strcpy(args[i][j], argument);
         }
     }
-    // for (int i = 0; i < comlim; i++){
+
+    // for (int i = 0; i < number_of_commands; i++)
+    // {
     //     if (args[i][0][0] == '\0')
     //         break;
-    //     for (int j = 0; j < arglim; j++)
-    //         printf("%s-", args[i][j]);
-    //     printf("\n");
+    //     for (int j = 0; j < numbers_of_arguments[i]; j++)
+    //     {
+    //         printf("%s ", args[i][j]);
+    //     }
+    //     printf("%d\n", numbers_of_arguments[i]);
     // }
+    // printf("\n");
+    run_query(args, number_of_commands, numbers_of_arguments);
 
-    run_query(query, number_of_commands);
+    // for (int i = 0; i < comlim; i++)
+    // {
+    //     for (int j = 0; j < arglim; j++)
+    //     {
+    //         free(args[i][j]);
+    //     }
+    //     free(args[i]);
+    // }
+    // free(args);
 }
 
 void read_file(char *filename, char **components, int queries[querylim][querylen], int lengths[2])
@@ -160,8 +234,8 @@ void read_file(char *filename, char **components, int queries[querylim][querylen
     }
 
     // Printing what we got
-    for (int i = 0; i < compi; i++)
-        printf("\"%s\" => %s.\n", comp_names[i], &components[i][1]);
+    // for (int i = 0; i < compi; i++)
+    //     printf("\"%s\" => %s.\n", comp_names[i], &components[i][1]);
 
     // Reading queries
     int queryi = 0;
@@ -182,12 +256,12 @@ void read_file(char *filename, char **components, int queries[querylim][querylen
         }
         queryi++;
     }
-    for (int i = 0; i < queryi; i++)
-    {
-        for (int j = 0; j < querylen; j++)
-            printf("%d ", queries[i][j]);
-        printf("\n");
-    }
+    // for (int i = 0; i < queryi; i++)
+    // {
+    //     for (int j = 0; j < querylen; j++)
+    //         printf("%d ", queries[i][j]);
+    //     printf("\n");
+    // }
     // Freeing memory
     for (int i = 0; i < compi; i++)
         free(comp_names[i]);
